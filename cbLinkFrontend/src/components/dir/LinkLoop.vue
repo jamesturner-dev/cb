5c2119fedf1f6cb331db95f34efdbdfb3635483f
jamesturner-dev <james@jamesturner.dev>
2022-11-01 11:30:26 -0400

    Directory View Dark MOde

--- cbLinkFrontend/src/components/dir/LinkLoop.vue
+++ cbLinkFrontend/src/components/dir/LinkLoop.vue
@@ -2,11 +2,12 @@
   <div class="flex w-full items-center justify-between space-x-6 p-5">
     <div class="flex-1 truncate">
       <div class="flex items-center space-x-3">
-        <h3 class="truncate text-sm font-medium text-gray-900">
+        <h3
+          class="truncate text-sm font-medium text-gray-900 dark:text-gray-400">
           <a :href="link.longUrl">{{ link.title }}</a>
         </h3>
         <span
-          class="cursor-pointer inline-block flex-shrink-0 rounded-full bg-purple-100 px-2 py-0.5 text-xs font-medium text-purple-800 hover:bg-purple-500 hover:text-white">
+          class="cursor-pointer inline-block flex-shrink-0 rounded-full bg-purple-100 px-2 py-0.5 text-xs font-medium text-purple-800 hover:bg-purple-500 hover:text-white hover:dark:bg-purple-700 dark:bg-purple-800 dark:text-purple-100">
           {{ link.category }}
         </span>
       </div>
@@ -14,8 +15,9 @@
         {{ link.description }}
       </p>
       <div class="text-xs text-gray-400 font-semibold">
+        <!-- This kinda thing has caused issued in the past so I used a function, but so far... -->
         Created on {{ link.createdAt.slice(0, 10) }} . Rated:
-        <span class="text-green-600">
+        <span class="text-purple-600">
           {{ link.rating }}
         </span>
       </div>
