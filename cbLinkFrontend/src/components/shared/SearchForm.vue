a0555cf564ea0c9ff00993a918966558c65036c5
jamesturner-dev <james@jamesturner.dev>
2022-11-06 09:02:31 -0500

    Search and search results implimented

--- cbLinkFrontend/src/components/shared/SearchForm.vue
+++ cbLinkFrontend/src/components/shared/SearchForm.vue
@@ -4,7 +4,7 @@
     <div
       class="flex flex-1 justify-between border-b border-gray-200 dark:border-gray-900">
       <div class="flex flex-1">
-        <form class="flex w-full md:ml-0" action="#" method="GET">
+        <form class="flex w-full md:ml-0" @submit.prevent="siteSearch">
           <label for="mobile-search-field" class="sr-only">Search</label>
           <label for="desktop-search-field" class="sr-only">Search</label>
           <div
@@ -12,30 +12,30 @@
             <div
               class="pointer-events-none absolute inset-y-0 left-2 flex items-center dark:bg-black">
               <MagnifyingGlassIcon
-                class="h-5 w-5 flex-shrink-0"
+                class="h-5 w-5 flex-shrink-0 cursor-pointer"
                 aria-hidden="true" />
             </div>
-            <input
-              name="mobile-search-field"
-              id="mobile-search-field"
-              class="h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:hidden"
-              placeholder="Search"
-              type="search" />
+
             <input
               name="desktop-search-field"
               id="desktop-search-field"
-              class="hidden h-full w-full dark:bg-black border-transparent py-2 pl-8 pr-3 text-sm text-gray-900 dark:text-gray-600 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:block"
+              class="h-full w-full dark:bg-black border-transparent py-2 pl-8 pr-3 text-sm text-gray-900 dark:text-gray-600 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:block"
               placeholder="Search the Salty Ape Empire"
+              v-model="searchTerm"
+              @keyup.enter="siteSearch"
               type="search" />
           </div>
         </form>
       </div>
       <div class="mr-4 flex items-center md:ml-6">
         <button
-          type="button"
+          type="submit"
           class="rounded-full bg-gray-100 dark:bg-gray-900 p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
-          <BellIcon class="h-6 w-6" aria-hidden="true" />
-          <span class="sr-only">View notifications</span>
+          <BellIcon
+            on-click="siteSearch()"
+            class="h-6 w-6"
+            aria-hidden="true" />
+          <span class="sr-only">Search Salty Ape</span>
         </button>
       </div>
     </div>
@@ -43,7 +43,14 @@
 </template>
 
 <script setup>
-// import { MagnifyingGlassIcon } from "@heroicons/vue/outline";
+import { ref } from "vue";
 import { BellIcon } from "@heroicons/vue/24/outline";
 import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+
+const searchTerm = ref("");
+
+const siteSearch = () => {
+  console.log("searching");
+  window.location.href = `/search/${searchTerm.value}`;
+};
 </script>
