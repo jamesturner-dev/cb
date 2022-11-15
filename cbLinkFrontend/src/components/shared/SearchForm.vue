b8a25abdd5ed6964309ac3781ceebf044b36fe7b
jamesturner-dev <james@jamesturner.dev>
2022-11-15 13:22:59 -0500

    improved search form

--- cbLinkFrontend/src/components/shared/SearchForm.vue
+++ cbLinkFrontend/src/components/shared/SearchForm.vue
@@ -5,13 +5,12 @@
       class="flex flex-1 justify-between border-b border-gray-200 dark:border-gray-900">
       <div class="flex flex-1">
         <form class="flex w-full md:ml-0" @submit.prevent="siteSearch">
-          <label for="mobile-search-field" class="sr-only">Search</label>
-          <label for="desktop-search-field" class="sr-only">Search</label>
+          <label for="search-field" class="sr-only">Search</label>
           <div
-            class="relative w-full text-gray-400 dark:bg-black focus-within:text-gray-600">
+            class="ml-2 relative w-full text-gray-400 dark:bg-black focus-within:text-gray-600">
             <div
               class="pointer-events-none absolute inset-y-0 left-2 flex items-center dark:bg-black">
-              <MagnifyingGlassIcon
+              <MagnifyingGlassCircleIcon
                 class="h-5 w-5 flex-shrink-0 cursor-pointer"
                 aria-hidden="true" />
             </div>
@@ -19,7 +18,7 @@
             <input
               name="desktop-search-field"
               id="desktop-search-field"
-              class="h-full w-full dark:bg-black border-transparent py-2 pl-8 pr-3 text-sm text-gray-900 dark:text-gray-600 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:block"
+              class="h-full w-full dark:bg-black border-transparent py-2 pl-8 pr-3 text-gray-900 dark:text-gray-600 placeholder-gray-300 dark:placeholder-gray-800 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:block"
               placeholder="Search cb.link"
               v-model="searchTerm"
               @keyup.enter="siteSearch"
@@ -27,15 +26,13 @@
           </div>
         </form>
       </div>
-      <div class="mr-4 flex items-center md:ml-6">
-        <button
-          type="submit"
+      <div class="mx-5 flex items-center">
+        <button type="submit"
           class="rounded-full bg-gray-100 dark:bg-gray-900 p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
-          <BellIcon
-            on-click="siteSearch()"
-            class="h-6 w-6"
+          <ChevronDoubleRightIcon
+            @click="siteSearch"
+            class="h-3 w-3"
             aria-hidden="true" />
-          <span class="sr-only">Search cb.link</span>
         </button>
       </div>
     </div>
@@ -44,13 +41,16 @@
 
 <script setup>
 import { ref } from "vue";
-import { BellIcon } from "@heroicons/vue/24/outline";
-import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
-
+import { ChevronDoubleRightIcon } from "@heroicons/vue/24/outline";
+import { MagnifyingGlassCircleIcon } from "@heroicons/vue/20/solid";
+import Swal from "sweetalert2";
 const searchTerm = ref("");
 
 const siteSearch = () => {
-  console.log("searching");
-  window.location.href = `/search/${searchTerm.value}`;
+  if(searchTerm){
+    console.log("searching");
+    window.location.href = `/search/${searchTerm.value}`;
+  } 
 };
 </script>
+
