0821a362bb2284bece2ad22e06f457992f6dc5fe
jamesturner-dev <james@jamesturner.dev>
2022-11-16 08:34:10 -0500

    Help Section

--- cbLinkFrontend/src/components/page/HelpPage.vue
+++ cbLinkFrontend/src/components/page/HelpPage.vue
@@ -1,66 +1,16 @@
 <template>
-  <div class="relative overflow-hidden py-10">
-    <div class="relative px-4 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-prose text-lg">
-        <h1>
-          <span class="block text-center text-lg font-semibold text-purple-700">
-            About Salty Ape's cb.link
-          </span>
-          <span
-            class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-700 sm:text-4xl">
-            What is this all about then?
-          </span>
-        </h1>
-        <p class="mt-5 text-xl leading-8 text-gray-500">
-          Not too long ago I got fired from my job.
-          <span class="text-purple-700"> Without cause</span>. The worse part
-          about it is that I could not continue working on the project I was the
-          sole developer on. You know, it was
-          <span class="text-purple-700">my</span> project.
-        </p>
-
-        <p class="text-gray-600 font-bold mt-10 text-xl">
-          Not legally of course, so I had to let it go.
-        </p>
-
-        <p class="text-gray-500 dark:gray-300 mt-5">
-          I had a couple of projects I had started before that gig, one being
-          <a class="text-purple-700" href="https://headlin3s.com">
-            Headlin3s
-          </a>
-          but I wanted to create something new and fresh. That did things the
-          way I wanted them to be done each step of the way. I also wanted to
-          create something that I could get posted before my severance ran out
-          as a resume piece.
-        </p>
-
-        <p class="text-gray-500 mt-5">
-          I decided to create a system to track all the links from my various
-          web projects. I decided to go ahead and impliment a short url type of
-          scheme, and then I thought why not create some directory type views of
-          the links?
-        </p>
-
-        <h2 class="text-gray-600 font-bold my-5 text-xl">
-          It would be cooler if it was multi user
-        </h2>
-
-        <p class="text-gray-500">
-          It still felt a little flimsy so I thought - why not make it multi
-          user so other people could use it to create their own directory and
-          manage their own links? And so cb.link was born.
-        </p>
-        <p class="text-gray-500 my-5">
-          If nothing else it does show how I approach a full stack project. You
-          can
-          <a
-            class="text-purple-700"
-            href="https://github.com/jamesturner-dev/cb"
-            >read more about it here as well as check out the code itself</a
-          >
-          .
-        </p>
-      </div>
-    </div>
+  <div class="relative overflow-hidden py-5">
+    <PageHeader :content="content" />
+    <HelpQuestions />
   </div>
 </template>
+
+<script setup>
+import HelpQuestions from './HelpQuestions.vue';
+import PageHeader from './PageHeader.vue';
+const content = [
+  "Click on the question to reveal the answer", 
+  "cb.link Help Page"
+];
+</script>
+
