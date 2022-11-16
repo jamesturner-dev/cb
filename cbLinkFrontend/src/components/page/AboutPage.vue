30d00019fa52e3e37ae812f329364833929cf746
jamesturner-dev <james@jamesturner.dev>
2022-11-16 08:34:27 -0500

    help section

--- cbLinkFrontend/src/components/page/AboutPage.vue
+++ cbLinkFrontend/src/components/page/AboutPage.vue
@@ -1,68 +1,65 @@
+<script setup>
+import PageHeader from './PageHeader.vue';
+const content = [
+  "About Salty Ape's cb.link", 
+  "What is this all about then?"
+];
+</script>
+
 <template>
-  <div class="relative overflow-hidden py-10">
-    <div class="relative px-4 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-prose text-lg">
-        <h1>
-          <span class="block text-center text-lg font-semibold text-purple-700">
-            About Salty Ape's cb.link
-          </span>
-          <span
-            class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-700 sm:text-4xl">
-            What is this all about then?
-          </span>
-        </h1>
-        <img
-          class="h-64 mx-auto mt-8"
-          src="https://imagedelivery.net/jUv0WjkQAcJxE0kRYIap3Q/38e6afbc-8036-4227-a33e-81ad5db8d100/public" />
-        <p class="mt-5 text-xl leading-8 text-gray-500">
-          Not too long ago I got fired from my job.
-          <span class="text-purple-700"> Without cause</span>. The worst part
-          about it was that I could not continue working on the project I was
-          the sole developer on. You know, it was
-          <span class="text-purple-700">my</span> project.
-        </p>
+  <div class="relative overflow-hidden p-5">
 
-        <p class="text-gray-600 font-bold mt-10 text-xl">
-          Not legally of course, so I had to let it go.
-        </p>
+    <div class="mx-auto max-w-prose text-lg">
 
-        <p class="text-gray-500 dark:gray-300 mt-5">
-          I had a couple of projects I had started before that gig, one being
-          <a class="text-purple-700" href="https://headlin3s.com">
-            Headlin3s
-          </a>
-          but I wanted to create something new and fresh the way I wanted to do
-          it. I also wanted to create something that I could get posted before
-          my severance ran out as a resume piece.
-        </p>
+    <PageHeader :content="content" />
+      
+      <p class="mt-10 text-xl leading-8 text-gray-500">
+        Not too long ago I got fired from my job.
+        <span class="text-purple-700"> Without cause</span>. The worst part
+        about it was that I could not continue working on the project I was
+        the sole developer on. You know, it was
+        <span class="text-purple-700">my</span> project.
+      </p>
 
-        <p class="text-gray-500 mt-5">
-          I decided to create a system to track all the links from my various
-          web projects. I decided to go ahead and implement a short url type of
-          scheme, and then I thought why not create some directory type views of
-          the links?
-        </p>
+      <p class="text-gray-600 font-bold mt-10 text-xl">
+        Not legally of course, so I had to let it go.
+      </p>
 
-        <h2 class="text-gray-600 font-bold my-5 text-xl">
-          It would be cooler if it was multi-user
-        </h2>
+      <p class="text-gray-500 dark:gray-300 mt-5">
+        I had a couple of projects I had started before that gig, one being
+        <a class="text-purple-700" href="https://headlin3s.com">
+          Headlin3s
+        </a>
+        but I wanted to create something new and fresh the way I wanted to do
+        it. I also wanted to create something that I could get posted before
+        my severance ran out as a resume piece.
+      </p>
 
-        <p class="text-gray-500">
-          It still felt a little flimsy so I thought - why not make it multi
-          user so other people could use it to create their own directory and
-          manage their own links?
-        </p>
-        <p class="text-gray-500 my-5">
-          If nothing else it does show how I approach a full stack project. You
-          can
-          <a
-            class="text-purple-700"
-            href="https://github.com/jamesturner-dev/cb">
-            read more about it here as well as check out the code itself
-          </a>
-          .
-        </p>
-      </div>
+      <p class="text-gray-500 mt-5">
+        I decided to create a system to track all the links from my various
+        web projects. I decided to go ahead and implement a short url type of
+        scheme, and then I thought why not create some directory type views of
+        the links?
+      </p>
+
+      <h2 class="text-gray-600 font-bold my-5 text-xl">
+        It would be cooler if it was multi-user
+      </h2>
+
+      <p class="text-gray-500">
+        It still felt a little flimsy so I thought - why not make it multi
+        user so other people could use it to create their own directory and
+        manage their own links?
+      </p>
+      <p class="text-gray-500 my-5">
+        If nothing else it does show how I approach a full stack project. You
+        can
+        <a class="text-purple-700" href="https://github.com/jamesturner-dev/cb">
+          read more about it here as well as check out the code itself
+        </a>
+        .
+      </p>
     </div>
+
   </div>
 </template>
---
+++ cbLinkFrontend/src/components/page/HelpQuestions.vue
@@ -0,0 +1,58 @@
+<template>
+
+  <div class="w-full">
+    <div class="mx-auto w-full max-w-lg rounded-2xl mt-10 p-2">
+      <Disclosure v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 dark:bg-opacity-10 dark:text-gray-600 px-4 py-2 text-left text-sm font-medium text-purple-900  hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          <span>Can I get this code?</span>
+          <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-purple-500" />
+        </DisclosureButton>
+        <DisclosurePanel class="p-5 pb-2 text-sm text-gray-500">
+          <p class="mb-5">
+            Yes, you can get the code for this site at
+            <a class="text-purple-700" href="https://github.com/jamesturner-dev/cb">
+              the github repository.</a> If you just want a directory of your own you can
+            sign up for <a class="text-purple-700" href="http://localhost:5173/user/register">a free account</a> and get
+            started right away.
+          </p>
+        </DisclosurePanel>
+      </Disclosure>
+      <Disclosure as="div" class="mt-2" v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 dark:bg-opacity-10 dark:text-gray-600 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          <span>Do you offer technical support?</span>
+          <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-purple-500" />
+        </DisclosureButton>
+        <DisclosurePanel class="p-5 pb-2 text-sm text-gray-500">
+          <p class="mb-5"> Sure. I do consulting for BTC. You can
+            <a href="#" class="text-purple-700">click here</a> to book a session with me. I can help you with your
+            project, or just answer questions about the code.
+          </p>
+        </DisclosurePanel>
+      </Disclosure>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import { ChevronUpIcon } from '@heroicons/vue/20/solid'
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/vue'
+
+const help = [
+  {
+    question: "What is your refund policy?",
+    answer: "If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked."
+  },
+  {
+    question: "Do you offer technical support?",
+    answer: "Did you try turning it off and back on again?"
+  }
+]; 
+</script>
